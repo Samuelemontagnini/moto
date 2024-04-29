8e17a9308a98a8d0a13793e194cf6649837c595f
Samuelemontagnini <93184581+Samuelemontagnini@users.noreply.github.com>
2024-04-29 22:46:49 +0200

    Add files via upload

--- progetto info moto final/app.py
+++ progetto info moto final/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
+from flask import render_template, redirect, url_for
+from flask_login import login_required, current_user
 import os
 import sqlite3
 
@@ -17,18 +19,21 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 
 class User(UserMixin):
-    def __init__(self, id, username):
+    def __init__(self, id, username, email, data_di_registrazione):
         self.id = id
         self.username = username
+        self.email = email
+        self.data_di_registrazione = data_di_registrazione
 
 @login_manager.user_loader
 def load_user(user_id):
     conn = get_db_connection()
-    user = conn.execute('SELECT * FROM utenti WHERE id = ?', (user_id,)).fetchone()
+    user = conn.execute('SELECT id, username, email, data_di_registrazione FROM utenti WHERE id = ?', (user_id,)).fetchone()
     conn.close()
-    if user is None:
-        return None
-    return User(user['id'], user['username'])
+    if user:
+        return User(user['id'], user['username'], user['email'], user['data_di_registrazione'])
+    return None
+
 
 def get_db_connection():
     conn = sqlite3.connect('moto.db')
@@ -55,7 +60,7 @@ def login():
         user = conn.execute('SELECT * FROM utenti WHERE username = ?', (username,)).fetchone()
         conn.close()
         if user and check_password_hash(user['password'], password):
-            user_obj = User(user['id'], user['username'])
+            user_obj = User(user['id'], user['username'], user['email'], user['data_di_registrazione'])  # Passa anche l'email e la data di registrazione
             login_user(user_obj)
             return redirect(url_for('index'))
         flash('Credenziali non valide.')
@@ -71,15 +76,23 @@ def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+        email = request.form['email']  # Aggiungi la raccolta dell'email dal form
         password_hash = generate_password_hash(password)
         conn = get_db_connection()
         try:
-            conn.execute('INSERT INTO utenti (username, password) VALUES (?, ?)', (username, password_hash))
+            # Modifica la query per includere la colonna email
+            conn.execute('INSERT INTO utenti (username, password, email) VALUES (?, ?, ?)', (username, password_hash, email))
             conn.commit()
-        except sqlite3.IntegrityError:
-            flash('Username già in uso.')
+        except sqlite3.IntegrityError as e:
+            if 'UNIQUE constraint failed: utenti.username' in str(e):
+                flash('Username già in uso.')
+            elif 'UNIQUE constraint failed: utenti.email' in str(e):
+                flash('Email già in uso.')
+            else:
+                flash('Errore di registrazione.')
             return render_template('register.html')
-        conn.close()
+        finally:
+            conn.close()
         return redirect(url_for('login'))
     return render_template('register.html')
 
@@ -90,20 +103,15 @@ def add_moto():
         modello = request.form['modello']
         cilindrata = request.form['cilindrata']
         cv = request.form['cv']
+        velocita_massima =request.form['velocita_massima']
         image_file = request.files['image']
         if image_file and allowed_file(image_file.filename):
             filename = secure_filename(image_file.filename)
-            if not os.path.exists(app.config['UPLOAD_FOLDER']):
-                os.makedirs(app.config['UPLOAD_FOLDER'])
-            try:
-                image_file.save(os.path.join(app.config['UPLOAD_FOLDER'] + filename))
-                print(app.config['UPLOAD_FOLDER'], filename)
-            except Exception as e:
-                return f"An error occurred while saving the file: {e}"
+            image_file.save(os.path.join(app.config['UPLOAD_FOLDER'] + filename))
+            print(app.config['UPLOAD_FOLDER'], filename)
             conn = get_db_connection()
-            conn.execute('INSERT INTO moto (modello, cilindrata, cv, image_path) VALUES (?, ?, ?, ?)',
-                         (modello, cilindrata, cv, filename))
-            
+            conn.execute('INSERT INTO moto (modello, cilindrata, cv, velocita_massima, image_path) VALUES (?, ?, ?, ?, ?)',
+                         (modello, cilindrata, cv, int(velocita_massima), (app.config['UPLOAD_FOLDER'] + filename)[8:]))
             conn.commit()
             conn.close()
             return redirect(url_for('index'))
@@ -129,6 +137,87 @@ def add_tracciato():
             conn.close()
             return redirect(url_for('index'))
     return render_template('add_tracciato.html')
+@app.route('/profile')
+@login_required
+def profile():
+    # Assicurati che l'utente sia loggato, grazie a `login_required`
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+    # Qui passiamo l'oggetto `current_user` al template, che contiene le informazioni dell'utente loggato
+    return render_template('profile.html', user=current_user)
+
+@app.route('/commenti/<int:moto_id>', methods=['GET', 'POST'])
+@login_required
+def commenti(moto_id):
+    conn = get_db_connection()
+    # Modifica la query per includere una JOIN con la tabella utenti
+    com = conn.execute(
+        '''
+        SELECT commenti.*, utenti.username FROM commenti
+        JOIN utenti ON commenti.utente_id = utenti.id
+        WHERE commenti.moto_id = ?
+        ''', (moto_id, )
+    ).fetchall()
+
+    moto = conn.execute(
+        '''
+        SELECT * FROM moto WHERE id = ?
+        ''', (moto_id, )
+    ).fetchone()
+
+    conn.close()
+    
+    return render_template('commenti.html', commenti_moto=com, moto=moto)
+
+
+@app.route('/commenti_tracciato/<int:tracciato_id>', methods=['GET', 'POST'])
+@login_required
+def commenti_tracciato(tracciato_id):
+    conn = get_db_connection()
+    com = conn.execute(
+        '''
+        SELECT * FROM commenti WHERE tracciato_id = ?
+        ''', (tracciato_id, )
+    ).fetchall()
+
+    tracciato = conn.execute(
+        '''
+        SELECT * FROM tracciati WHERE id = ?
+        ''', (tracciato_id, )
+    ).fetchone()
+    
+
+    conn.commit()
+    conn.close()
+    
+    return render_template('commenti_tracciato.html', commenti_tracciato = com, tracciato = tracciato)
+
+@app.route('/aggiungi_commento_moto/<int:moto_id>', methods=['POST'])
+@login_required
+def aggiungi_commento_moto(moto_id):
+    testo = request.form.get('testo')
+    conn = get_db_connection()
+    conn.execute('INSERT INTO commenti (utente_id, moto_id, testo) VALUES (?, ?, ?)',
+                 (current_user.id, moto_id, testo))
+    conn.commit()
+    conn.close()
+    flash('Il tuo commento è stato aggiunto.')
+    return redirect(url_for('commenti', moto_id=moto_id))
+
+@app.route('/aggiungi_commento_tracciato/<int:tracciato_id>', methods=['POST'])
+@login_required
+def aggiungi_commento_tracciato(tracciato_id):
+    testo = request.form.get('testo')
+    conn = get_db_connection()
+    conn.execute('INSERT INTO commenti (utente_id, tracciato_id, testo) VALUES (?, ?, ?)',
+                 (current_user.id, tracciato_id, testo))
+    conn.commit()
+    conn.close()
+    flash('Il tuo commento è stato aggiunto.')
+    return redirect(url_for('commenti_tracciato', tracciato_id=tracciato_id))
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- progetto info moto final/db.sql
+++ progetto info moto final/db.sql
@@ -1,32 +1,100 @@
+-- Creazione della tabella moto con la colonna aggiuntiva per l'immagine e la velocità massima
 CREATE TABLE moto (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     modello TEXT NOT NULL,
     cilindrata TEXT NOT NULL,
-    cv TEXT NOT NULL
+    cv TEXT NOT NULL,
+    image_path TEXT,
+    velocita_massima INT
 );
 
+-- Creazione della tabella tracciati con la colonna aggiuntiva per l'immagine
 CREATE TABLE tracciati (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome TEXT NOT NULL,
     lunghezza REAL NOT NULL,
     curvatura INTEGER NOT NULL,
-    nazione TEXT NOT NULL
+    nazione TEXT NOT NULL,
+    image_path TEXT
 );
 
+-- Creazione della tabella utenti
 CREATE TABLE utenti (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    data_di_registrazione TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-ALTER TABLE moto ADD COLUMN image_path TEXT;
-ALTER TABLE tracciati ADD COLUMN image_path TEXT;
+
+-- Creazione della tabella commenti
+CREATE TABLE commenti (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    utente_id INTEGER NOT NULL,
+    moto_id INTEGER,
+    tracciato_id INTEGER,
+    testo TEXT NOT NULL,
+    data_ora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (utente_id) REFERENCES utenti(id),
+    FOREIGN KEY (moto_id) REFERENCES moto(id),
+    FOREIGN KEY (tracciato_id) REFERENCES tracciati(id)
+);
 
 
 
-INSERT INTO tracciati (nome, lunghezza, curvatura, nazione, image_path)
-VALUES ('Monza Circuit', 5.793, 11, 'Italia', 'images/tracks/monza_circuit.jpg');
 
-INSERT INTO moto (modello,cilindrata,cv, image_path)
-VALUES ('ktm','450','35', '450.jpg');
+
+INSERT INTO tracciati (nome, lunghezza, curvatura, nazione, image_path) VALUES ('Monza Circuit', 5.793, 11, 'Italia', 'images/tracks/monza_circuit.jpg');
+INSERT INTO tracciati (nome, lunghezza, curvatura, nazione, image_path) VALUES ('Stelvio Pass', 24.3, 48, 'Italia', 'images/tracks/passo_dello_stelvio.jpg');
+INSERT INTO tracciati (nome, lunghezza, curvatura, nazione, image_path) VALUES ('Laguna Seca', 3.602, 11, 'USA', 'images/tracks/laguna_seca.jpg');
+INSERT INTO tracciati (nome, lunghezza, curvatura, nazione, image_path) VALUES ('Imola Circuit', 4.909, 17, 'Italia', 'images/tracks/imola_circuit.jpg');
+INSERT INTO tracciati (nome, lunghezza, curvatura, nazione, image_path) VALUES ('Passo dello Stelvio', 20.7, 60, 'Italia', 'images/tracks/passo_dello_stelvio.jpg');
+INSERT INTO tracciati (nome, lunghezza, curvatura, nazione, image_path) VALUES ('Nürburgring Nordschleife', 20.832, 154, 'Germania', 'images/tracks/nurburgring_nordschleife.jpg');
+INSERT INTO tracciati (nome, lunghezza, curvatura, nazione, image_path) VALUES ('Passo di Gavia', 25.6, 30, 'Italia', 'images/tracks/passo_di_gavia.jpg');
+INSERT INTO tracciati (nome, lunghezza, curvatura, nazione, image_path) VALUES ('Circuit de Spa-Francorchamps', 7.004, 19, 'Belgio', 'images/tracks/spa_francorchamps.jpg');
+INSERT INTO tracciati (nome, lunghezza, curvatura, nazione, image_path) VALUES ('Passo del Mortirolo', 12.4, 33, 'Italia', 'images/tracks/passo_del_mortirolo.jpg');
+INSERT INTO tracciati (nome, lunghezza, curvatura, nazione, image_path) VALUES ('Silverstone Circuit', 5.891, 18, 'Regno Unito', 'images/tracks/silverstone_circuit.jpg');
+INSERT INTO tracciati (nome, lunghezza, curvatura, nazione, image_path) VALUES ('Passo del Bernina', 13, 13, 'Svizzera', 'images/tracks/passo_del_bernina.jpg');
+
+
+INSERT INTO moto (modello,cilindrata,cv, velocita_massima,image_path) VALUES ('ktm','450','35', '190','images/motos/450.jpg');
+INSERT INTO moto (modello, cilindrata, cv, velocita_massima, image_path) VALUES ('KTM SX', '250', '45', '170', 'images/motos/ktm_sx.jpg');
+INSERT INTO moto (modello, cilindrata, cv, velocita_massima, image_path) VALUES ('Ducati Panigale V4', '1103', '214', '300', 'images/motos/ducati_panigale_v4.jpg');
+INSERT INTO moto (modello, cilindrata, cv, velocita_massima, image_path) VALUES ('Yamaha YZF-R1', '998', '200', '285', 'images/motos/yamaha_yzf_r1.jpg');
+INSERT INTO moto (modello, cilindrata, cv, velocita_massima, image_path) VALUES ('Honda CBR600RR', '599', '120', '250', 'images/motos/honda_cbr600rr.jpg');
+INSERT INTO moto (modello, cilindrata, cv, velocita_massima, image_path) VALUES ('Suzuki GSX-R1000', '999', '199', '290', 'images/motos/suzuki_gsx_r1000.jpg');
+INSERT INTO moto (modello, cilindrata, cv, velocita_massima, image_path) VALUES ('BMW S1000RR', '999', '207', '299', 'images/motos/bmw_s1000rr.jpg');
+INSERT INTO moto (modello, cilindrata, cv, velocita_massima, image_path) VALUES ('Aprilia RSV4', '1099', '217', '305', 'images/motos/aprilia_rsv4.jpg');
+INSERT INTO moto (modello, cilindrata, cv, velocita_massima, image_path) VALUES ('MV Agusta F4', '998', '205', '292', 'images/motos/mv_agusta_f4.jpg');
+INSERT INTO moto (modello, cilindrata, cv, velocita_massima, image_path) VALUES ('Triumph Daytona', '765', '128', '260', 'images/motos/triumph_daytona.jpg');
+INSERT INTO moto (modello, cilindrata, cv, velocita_massima, image_path) VALUES ('Kawasaki Ninja ZX-10R', '998', '203', '300', 'images/motos/kawasaki_ninja_zx_10r.jpg');
+
+INSERT INTO commenti (utente_id, moto_id, testo) VALUES 
+(1, 1, 'Incredibilmente agile e veloce, un piacere da guidare.'),
+(1, 1, 'Ottimo maneggevolezza ma il comfort di guida potrebbe essere migliorato.');
+
+-- Commenti per la moto con id = 2
+INSERT INTO commenti (utente_id, moto_id, testo) VALUES 
+(1, 2, 'Perfetta per chi ama le velocità elevate e le prestazioni sportive.'),
+(1, 2, 'Il design è eccezionale, ma la manutenzione può essere costosa.');
+
+-- Commenti per la moto con id = 3
+INSERT INTO commenti (utente_id, moto_id, testo) VALUES 
+(1, 3, 'Un vero mostro di potenza, ideale per gli amanti delle prestazioni pure.'),
+(1, 3, 'La Ducati Panigale V4 non delude mai, dal sound alla velocità.');
+
+INSERT INTO commenti (utente_id, tracciato_id, testo) VALUES 
+(1, 1, 'Un classico tracciato pieno di storia, sempre un piacere correrci.'),
+(1, 1, 'Perfetto per testare le capacità tecniche in curva.');
+
+-- Commenti per il tracciato con id = 2
+INSERT INTO commenti (utente_id, tracciato_id, testo) VALUES 
+(1, 2, 'Uno dei passaggi montani più emozionanti e sfidanti d\'Europa.'),
+(1, 2, 'Ottimo per allenarsi per gare di resistenza, ma attenzione alle condizioni meteorologiche variabili.');
+
+
+INSERT INTO commenti (utente_id, tracciato_id, testo) VALUES 
+(1, 3, 'Laguna Seca è famoso per il suo "Corkscrew", unico e adrenalico.'),
+(1, 3, 'Tracciato corto ma intenso, con eccellenti opportunità di sorpasso.');
 
 
